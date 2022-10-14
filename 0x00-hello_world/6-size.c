7cba5666e9738522c8411873f325c92150a4a16b
deboski <dtidowu@gmail.com>
2022-10-14 09:02:52 -0700

    my seventh commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,42 @@
  */
 int main(void)
 {
-printf("Size of a char: %i byte(s)\m", sizeof(char));
-printf("Size of an int: %i byte(s)\m", sizeof(int)); 
-printf("Size of a long int: %i byte(s)\m", sizeof(long int));
-printf("Size of a long long int: %i byte(s)\m", sizeof(long long int));
-printf("Size of a float: %i byte(s)\m", sizeof(float)); 
-return (0)       
+	printf("Size of a char: %lu\n",  (unsigned long)sizeof(a));
+	printf("Size of an int: %lu\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu\n", (unsigned long)sizeof(long int));
+	printf("Size of a long ong int: %lu\n", (unsigned long)sizeof(long long int));
+printf("Size of a float: %lu\n", (unsigned long)sizeof(float);
+
+		return (0);
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
